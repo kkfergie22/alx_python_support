48aabf9bc6baca57b86669b860564c7369cc221c
Christian Koranteng <korantengchristian@gmail.com>
2024-02-02 17:25:44 +0000

    My first

---
+++ variables.py
@@ -0,0 +1,41 @@
+# Variable declaration and assignment
+x = 5
+name = "John"
+is_true = True
+
+# Basic data types
+num = 10
+pi = 3.14
+message = "Hello, world!"
+is_valid = True
+
+print(type(num))
+print(type(pi))
+print(type(message))
+print(type(is_valid))
+
+# Arithmetic operations
+x = 10
+y = 3
+sum_result = x + y
+division_result = x / y
+
+print(sum_result)
+print(division_result)
+
+# Code Challenges and Practice:
+
+# Challenge 1:
+
+# Write a program that calculates the average of three exam scores stored in variables.
+
+# Challenge 2:
+
+# def calculate_avg(score1,score2,score3):
+#     total = score1 + score2 + score3
+#     avg = total / 3
+#     return avg
+
+# print(calculate_avg(1,2,3))
+
+# Create a program that asks the user for their name and age, then prints out a message including both pieces of information.
